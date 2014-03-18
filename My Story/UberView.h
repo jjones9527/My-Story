da139109240037d3d26f14d6aaca8b11a54dccd5
Jeremy <jjones9527@gmail.com>
2014-03-17 21:32:33 -0400

    Initial Commit

---
+++ My Story/UberView.h
@@ -0,0 +1,14 @@
+//
+//  UberView.h
+//  My Story
+//
+//  Created by Jeremy Jones on 2014-03-17.
+//  Copyright (c) 2014 Jeremy Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UberView : UIViewController
+@property (strong, nonatomic) IBOutlet UIWebView *webView;
+
+@end
